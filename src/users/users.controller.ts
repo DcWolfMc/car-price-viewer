5fb59a1d012052d8e3413064dad6e9c732aeea5e
Daniel <danielmcolares@gmail.com>
2024-07-09 16:28:43 -0300

    routes for others user services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Query,
+  Param,
+  ParseIntPipe,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create.user.dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { UpdateUserDTO } from './dtos/update.user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,5 +21,25 @@ export class UsersController {
   @Post('/signup')
   create(@Body() user: CreateUserDTO) {
     return this.UserService.create(user.email, user.password);
+  }
+  @Get('/:id')
+  findUser(@Param('id', ParseIntPipe) id: number) {
+    return this.UserService.findOne(id);
+  }
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.UserService.find(email);
+  }
+  @Patch('/:id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() UpdateData: UpdateUserDTO,
+  ) {
+    return this.UserService.update(id, UpdateData);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id', ParseIntPipe) id: number) {
+    return this.UserService.remove(id);
   }
 }
